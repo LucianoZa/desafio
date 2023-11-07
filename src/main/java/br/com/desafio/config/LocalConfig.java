3f8fe4686e12ac196a910ed989ae2cc011b89e34
LucianoZa <58642882+LucianoZa@users.noreply.github.com>
2023-11-07 11:57:08 -0300

    Configuracao Local e Popula tabela Pauta para testes

---
+++ src/main/java/br/com/desafio/config/LocalConfig.java
@@ -0,0 +1,29 @@
+package br.com.desafio.config;
+
+import br.com.desafio.model.RqPautaAdd;
+import br.com.desafio.repository.PautaDAOImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private PautaDAOImpl pautaDAO;
+    @Bean
+    public void starDB() {
+        RqPautaAdd pauta1 = new RqPautaAdd("Pauta p1");
+        RqPautaAdd pauta2 = new RqPautaAdd("Pauta p2");
+        RqPautaAdd pauta3 = new RqPautaAdd("Pauta p3");
+        RqPautaAdd pauta4 = new RqPautaAdd("Pauta p4");
+
+        pautaDAO.addPauta(pauta1);
+        pautaDAO.addPauta(pauta2);
+        pautaDAO.addPauta(pauta3);
+        pautaDAO.addPauta(pauta4);
+
+    }
+}
--- src/main/java/br/com/desafio/service/IpautaService.java
+++ src/main/java/br/com/desafio/service/IpautaService.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface IpautaService {
 
      public RsPautaAdd addPauta(RqPautaAdd rqPautaAdd);
-
      public List<Pauta> getPauta(String codPauta, RqPautaGet rqPautaGet, Pageable pageable);
 
 }
