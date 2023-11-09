9aeed08b2120c810f3ab0cf6b9a4872b20bf6b3e
LucianoZa <58642882+LucianoZa@users.noreply.github.com>
2023-11-09 08:36:05 -0300

    Testes Application

---
+++ src/test/java/br/com/desafio/DesafioApplicationTest.java
@@ -0,0 +1,12 @@
+package br.com.desafio;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTest {
+//    @Test
+//    void main(){
+//       DesafioApplication.main(new String[]{});
+//    }
+
+}
